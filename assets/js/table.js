0adef7d2cdebb5be6047bc8f572257a46ae06cfe
Anil Bostanci <anil.bostanci@v-count.com>
2021-11-16 02:36:35 +0300

    prettified table.js

--- assets/js/table.js
+++ assets/js/table.js
@@ -1,70 +1,68 @@
 import Axios from "axios";
 export default {
-  data() {
-    return {
-      perPage: 10,
-      currentPage: 1,
-      items: [],
-      search: '',
-      table: []
-    }
-  },
-  mounted() {
-    console.log("is auth?")
-
-    
-    Axios.get('/api/tables')
-      .then(response => {
-        /*
-        if (response.data["success"]) {
-          this.result_matrix = this.generateString(response.data["result_stringified"]);
+    data() {
+        return {
+            perPage: 10,
+            currentPage: 1,
+            items: [],
+            search: '',
+            table: []
         }
-        else {
-          this.backend_error = response.data["error"];
-        }
-        */
-        this.items = response.data["data"];
-        /*let tableElement = document.querySelector("div[data-table]");
-        let table = JSON.parse(tableElement.dataset.table);*/
-        this.table = response.data["data"];
-      });
-
-
-  },
-  computed: {
-    rows() {
-      return this.items.length;
-    }
-  },
-  watch: {
-    perPage(newValue) {
-      if (newValue < 1) {
-        this.perPage = 1
-      }
-      else {
-        this.perPage = newValue;
-      }
     },
-    search(newValue) {
-      if (newValue === '') {
-        this.items = this.table;
-      }
-      else {
-        const payload = { q: newValue };
-        Axios.post('/api/search', payload)
-          .then(response => {
-            /*
-            if (response.data["success"]) {
-              this.result_matrix = this.generateString(response.data["result_stringified"]);
-            }
-            else {
-              this.backend_error = response.data["error"];
-            }
-            */
-            this.items = response.data["data"];
-          });
+    mounted() {
+        console.log("is auth?")
 
-      }
+
+        Axios.get('/api/tables')
+            .then(response => {
+                /*
+                if (response.data["success"]) {
+                  this.result_matrix = this.generateString(response.data["result_stringified"]);
+                }
+                else {
+                  this.backend_error = response.data["error"];
+                }
+                */
+                this.items = response.data["data"];
+                /*let tableElement = document.querySelector("div[data-table]");
+                let table = JSON.parse(tableElement.dataset.table);*/
+                this.table = response.data["data"];
+            });
+
+
+    },
+    computed: {
+        rows() {
+            return this.items.length;
+        }
+    },
+    watch: {
+        perPage(newValue) {
+            if (newValue < 1) {
+                this.perPage = 1
+            } else {
+                this.perPage = newValue;
+            }
+        },
+        search(newValue) {
+            if (newValue === '') {
+                this.items = this.table;
+            } else {
+                const payload = { q: newValue };
+                Axios.post('/api/search', payload)
+                    .then(response => {
+                        /*
+                        if (response.data["success"]) {
+                          this.result_matrix = this.generateString(response.data["result_stringified"]);
+                        }
+                        else {
+                          this.backend_error = response.data["error"];
+                        }
+                        */
+                        this.items = response.data["data"];
+                    });
+
+            }
+        }
     }
-  }
 }
